0302eaeaec42ded05c36070b243b8a3f1624f477
Xavier Muller <xavier.muller@etu.u-bordeaux.fr>
2020-05-08 13:02:59 +0200

    Add temporary pacman package list -> to change into list file.

---
+++ .config/pacman.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# office
+sudo pacman -S --needed neovim evince texlive-core biber \
+	inkscape gimp \
+
+# programming
+sudo pacman -S --needed git cmake gcc clang \
+	python-pip ipython 
