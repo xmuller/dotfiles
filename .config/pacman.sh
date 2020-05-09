6f543233e8577a9b7f786a130d9fa4482cfe60e4
Xavier Muller <xavier.muller@etu.u-bordeaux.fr>
2020-05-09 06:41:27 +0200

    add packages

--- .config/pacman.sh
+++ .config/pacman.sh
@@ -3,11 +3,11 @@
 # office
 sudo pacman -S --needed neovim evince texlive-core biber \
 	inkscape gimp \
-	htop nmtui  \
+	htop network-manager  \
 
 
 
 
 # programming
-sudo pacman -S --needed git cmake gcc clang \
+sudo pacman -S --needed make git cmake gcc clang \
 	python-pip ipython 
